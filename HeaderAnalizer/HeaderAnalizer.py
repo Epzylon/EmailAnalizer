7f54c55755a44be898a7538e16a1de48377abe71
Gustavo Daniel Rodriguez <gustavo@netsecure.com.ar>
2016-03-08 12:31:21 -0300

    Initial Commit

---
+++ HeaderAnalizer/HeaderAnalizer.py
@@ -0,0 +1,17 @@
+'''
+Created on Mar 8, 2016
+
+@author: grodriguez
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
