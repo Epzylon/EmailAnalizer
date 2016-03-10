2c5537e337c6f8cf1801921cfbbc28a4eaa85050
Gustavo Daniel Rodriguez <gustavo@netsecure.com.ar>
2016-03-10 14:15:12 -0300

    Added self.values as a dict with a list

--- TokenAnalizer/Trace/received.py
+++ TokenAnalizer/Trace/received.py
@@ -7,6 +7,24 @@ Created on Mar 8, 2016
 import HeaderAnalizer.EmailTracertErrors.InvalidToken as InvalidToken
 import email.utils.parsedate as ParseDate
 
+class ExtendedDomain(object):
+    '''
+    The information related to the tokens FROM and BY is called an described
+    on the RFC 5321, section 4.4 as Extended-Domain in Augmented BNF notation
+    This object receive this strings and make the analisys/disection
+    '''
+    def __init__(self,value):
+        self.value = value.split()
+        #If the value has only one string, it must be the domain
+        if len(self.value) == 1:
+            self.domain = self.value[0]
+        else:
+            pass
+        
+    
+    
+        
+
 class Received(object):
     '''
     Aanalize the Received values and return a dic object
@@ -16,6 +34,9 @@ class Received(object):
     
     def __init__(self, received_value):
         self.received_value = received_value
+        
+        #Valid Tokens
+        tokens = ["from","by","via","with","id"]
                
         #Value must have two parts, FROM part and DATE part
         #divided by a colon
@@ -31,91 +52,22 @@ class Received(object):
             #Lets break the from_field in to a list
             from_list = from_field.split()
             
-            #Searching tokens
-            #BY tokens MUST exist and appears both on internal jump
-            #as well in normal jumps
-            by_found = from_list.count("by")
+            self.values = {
+                          'from':[],
+                          'by':[],
+                          'via':[],
+                          'with':[],
+                          'id':[]
+            }
             
-            #On internal jumps the token FROM doesn't appears (against RFC?)
-            from_found = from_list.count("from")
-            
-            #The following are optional tokens
-            via_found = from_list.count("via")
-            with_found = from_list.count("with")
-            id_found = from_list.count("id")
-            for_found = from_list.count("for")
-            
-            #This matrix will order the tokens once having the index value
-            m_received = []
-            
-            #Each received string must have a "BY" token
-            if by_found == 0:
-                raise InvalidToken(self.received_value,"BY")
-            else:
-                by_id = from_list.index("by")
-                m_received.append(['by', by_id])
-            
-            #FROM token appears on normal jumps, otherwise
-            #it is a internal jump
-            if from_found == 1:
-                from_id = from_list.find("from")
-                self.internal_jump = False
-                m_received.append(['from',from_id])
-            else:
+            for word in from_list:
+                if word in tokens:
+                    token_found = word
+                else:
+                    self.values[token_found].append(word)
+            if self.values['from'] == []:
                 self.internal_jump = True
-            
-            #Index values on optional tokens
-            if via_found == 1:
-                via_id = from_list.index("via")
-                m_received.append(['via',via_id])
-            
-            if with_found == 1:
-                with_id = from_list.index("with")
-                m_received.append(['with',with_id])
-                
-            if id_found == 1:
-                id_id = from_list.index("id")
-                m_received.append(['id',id_id])
-            
-            if for_found == 1:
-                for_id = from_list.index("for")
-                m_received.append(['for',for_id])
-            
-            m_received.sort(key=lambda x: x[1])
-            
-        if not self.internal_jump:
-            self.from_value = from_list[from_id+1:by_id]
-        
-            
+            else:
+                self.internal_jump = False
         else:
             raise InvalidToken(self.received_value,";")
-        
-        
-        
-    def getReceivedDict(self):
-        pass
-    
-    def getIP(self):
-        pass
-    
-    def getDomain(self):
-        pass
-    
-    def getTCPInfo(self):
-        pass
-    
-    def getOptInfo(self):
-        pass
-    
-    def getVia(self):
-        pass
-    
-    def getWith(self):
-        pass
-    
-    def getID(self):
-        pass
-    
-    def getFor(self):
-        pass
-    
