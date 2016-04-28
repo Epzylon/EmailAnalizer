83a7dde1d870e10938386297222a080b23e6fa72
Gustavo Daniel Rodriguez <gustavo@netsecure.com.ar>
2016-04-28 14:46:05 -0300

    One time headers

--- HeaderAnalizer/HeaderAnalizer.py
+++ HeaderAnalizer/HeaderAnalizer.py
@@ -4,6 +4,7 @@ Created on Mar 8, 2016
 '''
 from email import message_from_string as m_string
 from email.utils import parseaddr as address
+from email.utils import parsedate as date
 from HeaderAnalizer.EmailTracertErrors import NotMessageObject
 
 
@@ -20,7 +21,35 @@ class HeaderAnalizer(object):
         except:
             pass
         else:
-            self.from_address = address(self.message.get('from'))
+            #############################################
+            # Headers that should appears at most once  #
+            #############################################
+            
+            #Odd header            
+            self.orig_date = date(self.message.get('orig-date'))
+            
+            #Address type headers
+            self.sender = address(self.message.get('sender'))
+            self.reply_to = address(self.message.get('reply-to'))
+            
+            #ID type headers
+            self.message_id = self.message.get('message-id')
+            self.in_reply_to = self.message.get('in-reply-to')
+            self.references = self.message.get('references')
+            
+            #Subject
+            self.subject = self.message.get('subject')
+            
+            
+            #############################################
+            # Headers that could appears more than once #
+            #############################################
+            
+            #From,bcc,cc,reply-to
+            
+            
+
+            
             
 
        
