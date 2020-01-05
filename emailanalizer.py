aaf64eef50fd316dfca324354cf7cf09f5d6c9d8
GustavoRodriguez <gustavodaniel.rodriguez@bbva.com>
2020-01-05 10:07:57 -0300

    PEP8 Arrangements

--- emailanalizer.py
+++ emailanalizer.py
@@ -8,11 +8,11 @@ from TokenAnalizer.Trace.received import Received
 pname = 'emailanalizer'
 pdesc = "Analize email headers"
 
-parser = argparse.ArgumentParser(description=pdesc,prog=pname)
+parser = argparse.ArgumentParser(description=pdesc, prog=pname)
 
-parser.add_argument('-T','--tracert',help="Show the trace path of the email")
-parser.add_argument('-f','--file',dest='mail_file',
-                    action='store',help='Email file')
+parser.add_argument('-T', '--tracert', help="Show the trace path of the email")
+parser.add_argument('-f', '--file', dest='mail_file',
+                    action='store', help='Email file')
 
 args = parser.parse_args()
 
@@ -23,11 +23,9 @@ else:
     e = open_efile(open(args.mail_file))
     reclist = e.get_all('Received')
     reclist.reverse()
-    table = PrettyTable(['From','By','Date','With','Via','ID'])
+    table = PrettyTable(['From', 'By', 'Date', 'With', 'Via', 'ID'])
     for line in reclist:
         l = Received(line)
-        table.add_row([l.From,l.By,l.Date,l.With,l.Via,l.Id])
+        table.add_row([l.From, l.By, l.Date, l.With, l.Via, l.Id])
     print(table)
-        
 
-        
